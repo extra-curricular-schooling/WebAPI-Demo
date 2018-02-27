3c548ed48188559772ec012c833243b8e95d7ef1
Scott Roberts <s.e.roberts0@gmail.com>
2018-02-27 08:32:38 -0800

    JWT Test Project

---
+++ ECSDevServer/ECS.WebAPI.Services.Tests/JwtManagerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+using Moq;
+using ECS.WebAPI.Services;
+namespace ECS.WebAPI.Services.Tests
+{
+    public class JwtManagerTest
+    {
+        public class GenerateTokenWithoutSecretTest
+        {
+            [Fact]
+            public void PassShouldBeTheSameToken()
+            {
+                string token1 = JwtManager.GenerateTokenWithoutSecret("scott");
+                string token2 = JwtManager.GenerateTokenWithoutSecret("scott");
+                Assert.Same(token1, token2);
+            }
+        }
+        public class GenerateTokenTest
+        {
+            [Fact]
+            public void FailShouldNotBeTheSameToken()
+            {
+                string token1 = JwtManager.GenerateToken("scott");
+                string token2 = JwtManager.GenerateToken("scott");
+                Assert.NotSame(token1, token2);
+                
+            }
+        }
+    }
+}
