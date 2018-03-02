bf47e1cfff5828f2541a25d67e43910042760968
FurkanArif <35707731+FurkanArif@users.noreply.github.com>
2018-03-02 13:53:00 -0800

    EmbedContent
    solved a problem

--- ECSDevClient/src/components/pages/Home.vue
+++ ECSDevClient/src/components/pages/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="embed_content">
     <h1>Welcome to Article Page</h1>
-    <iframe src="https://ecschooling.org/" name="frame_result" align="right" height="700" width="1300" style="border:4px solid black;"></iframe>
-    <p><a href ="https://powerhookahus.com/" target="frame_result">Link 1</a></p>
-    <p><a href ="https://www.hookah-shisha.com/" target="frame_result">Link 2</a></p>
-    <p><a href ="http://www.ivc.edu/Pages/default.aspx" target="frame_result">Link 3 Education</a></p>
-    <vue-good-table :columns="columns" :onClick="onClickFn" :rows="rows" />
+    <iframe src="https://ecschooling.org/" name="frame_result" height="700" width=100% style="border:8px solid black;"></iframe>
+    <vue-good-table :columns="columns" :rows="rows" styleClass="table condensed table-bordered table-striped" paginate=true>
+    <template slot="table-row" slot-scope="props">
+    <td>{{ props.row.articleType }}</td>
+    <td>{{ props.row.articleTitle }}</td>
+    <td>{{ props.row.articleLink }}</td>
+    <td>{{ props.row.articleDescription }}</td>
+    <td>{{ props.row.articleTag }}</td>
+    <a v-bind:href="props.row.articleLink" target="frame_result">DISPLAY</a>
+    </template>
+    </vue-good-table>
   </div>
 </template>
 <script>
@@ -27,7 +33,8 @@ export default {
         },
         {
           label: 'Article Link',
-          field: 'articleLink'
+          field: 'articleLink',
+          html: true
         },
         {
           label: 'Article Description',
@@ -41,12 +48,16 @@ export default {
       rows: [
         {articleType: 'School', articleTitle: 'main page', articleLink: 'http://www.ivc.edu/Pages/default.aspx', articleDescription: 'Famous School', articleTag: 'education'},
         {articleType: 'Hookah', articleTitle: 'power', articleLink: 'https://www.hookah-shisha.com/', articleDescription: 'Famous', articleTag: 'entertainment'},
-        {articleType: 'King', articleTitle: 'Competition', articleLink: 'https://powerhookahus.com/', articleDescription: 'Famous Brands', articleTag: 'entertainment'}
-      ],
-      onClickFn: function (row, index) {
-        console.log(row)
-        console.log(index)
-      }
+        {articleType: 'King', articleTitle: 'Competition', articleLink: 'https://powerhookahus.com/', articleDescription: 'Famous Brands', articleTag: 'entertainment'},
+        {articleType: 'School', articleTitle: 'BeachBoard', articleLink: 'https://bbcsulb.desire2learn.com/d2l/login?logout=1', articleDescription: 'School', articleTag: 'education'},
+        {articleType: 'School', articleTitle: 'main page', articleLink: 'http://www.ivc.edu/Pages/default.aspx', articleDescription: 'Famous School', articleTag: 'education'},
+        {articleType: 'Lounge', articleTitle: 'Good Ones', articleLink: 'https://www.caspianrestaurant.com/hookah-lounge', articleDescription: 'Where I go To', articleTag: 'entertainment'},
+        {articleType: 'King', articleTitle: 'Competition', articleLink: 'https://www.texashookah.com/afzal.html', articleDescription: 'Famous Brands', articleTag: 'entertainment'},
+        {articleType: 'School', articleTitle: 'main page', articleLink: 'http://www.ivc.edu/Pages/default.aspx', articleDescription: 'Famous School', articleTag: 'education'},
+        {articleType: 'Hookah', articleTitle: 'power', articleLink: 'https://www.hookah-shisha.com/', articleDescription: 'Famous', articleTag: 'entertainment'},
+        {articleType: 'OC', articleTitle: 'Home', articleLink: 'https://www.yelp.com/', articleDescription: 'Famous OC', articleTag: 'Like'},
+        {articleType: 'Lounge', articleTitle: 'Good Ones', articleLink: 'http://myblufig.com/', articleDescription: 'Where I go To', articleTag: 'entertainment'}
+      ]
     }
   }
 }
---
+++ ECSDevClient/src/components/pages/Sweepstake.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Welcome to Sweepstakes.</h1>
+    <h1>Time Left</h1>
+    <Countdown deadline="March 22, 2018"></Countdown>
+    <!-- or
+    <Countdown end="August 22, 2022"></Countdown> -->
+    <button v-on:click="ticket_bought">Buy A Ticket</button>
+  </div>
+</template>
+<script>
+// import Vue from 'vue'
+import Countdown from 'vuejs-countdown'
+export default {
+  data: function () {
+    return {
+      points: 5
+    }
+  },
+  components: { Countdown },
+  methods: {
+    ticket_bought: function () {
+      alert('One Ticket Bought for ' + this.points)
+    }
+    // hehe: function () {
+    //   alert('you are stupid')
+    // }put if statements for points if points greater than 5 do this or else do that
+  }
+}
+</script>
+<style scoped>
+button {
+  background-color: #E8E9E4;
+  color: black;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 270px;
+  opacity: 0.9;
+}
+
+button:hover {
+    opacity:1;
+}
+</style>
