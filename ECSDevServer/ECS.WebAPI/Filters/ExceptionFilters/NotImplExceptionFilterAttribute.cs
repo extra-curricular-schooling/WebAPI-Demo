eea62d76ac40cd19e5984361792ed32db466735b
Scott Roberts <s.e.roberts0@gmail.com>
2018-03-14 23:47:39 -0700

    Added New Client File Structure
    The file structure folder is a temporary one and only supposed to use as a guide. Please do not build out of it. We need to modify the existing structure to match.

---
+++ ECSDevServer/ECS.WebAPI/Filters/ExceptionFilters/NotImplExceptionFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace ECS.WebAPI.Filters.ExceptionFilters
+{
+    // This is an example of a Exception filter we can put at the global, controller, or action levels.
+    public class NotImplExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if (context.Exception is NotImplementedException)
+            {
+                context.Response = new HttpResponseMessage(HttpStatusCode.NotImplemented);
+            }
+        }
+    }
+}
