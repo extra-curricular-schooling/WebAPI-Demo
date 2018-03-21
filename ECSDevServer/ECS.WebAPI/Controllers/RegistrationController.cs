d855b32c42fc5a2932900ab9379924d250b52093
Trisha Echual <piechual@gmail.com>
2018-03-21 04:43:50 -0700

    Retrieve Security Questions in Registration Controller
    Get real list of security questions from data store to package in get request to registration.

--- ECSDevServer/ECS.WebAPI/Controllers/RegistrationController.cs
+++ ECSDevServer/ECS.WebAPI/Controllers/RegistrationController.cs
@@ -6,18 +6,28 @@ using System.Net;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using ECS.Repositories;
+using System;
 
 namespace ECS.WebAPI.Controllers
 {
     public class RegistrationController : ApiController
     {
-        private readonly IAccountRepository accountRepository;
-        private readonly IUserRepository userRepository;
+        private readonly IAccountRepository _accountRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly ISecurityQuestionRepository _securityQuestionRepository;
 
         public RegistrationController()
         {
-            accountRepository = new AccountRepository();
-            userRepository = new UserRepository();
+            _accountRepository = new AccountRepository();
+            _userRepository = new UserRepository();
+            _securityQuestionRepository = new SecurityQuestionRepository();
+        }
+
+        public RegistrationController(IAccountRepository accountRepo, IUserRepository userRepo, ISecurityQuestionRepository securityQuestionRepo)
+        {
+            _accountRepository = accountRepo;
+            _userRepository = userRepo;
+            _securityQuestionRepository = securityQuestionRepo;
         }
 
         /// <summary>
@@ -83,17 +93,19 @@ namespace ECS.WebAPI.Controllers
                 LastName = registrationForm.LastName,
                 ZipLocations = zipLocationListObj
             };
-            userRepository.Insert(user);
+            _userRepository.Insert(user);
 
             Account account = new Account()
             {
                 UserName = registrationForm.Username,
+                Email = registrationForm.Email,
                 Password = registrationForm.Password,
                 AccountStatus = true,
+                SuspensionTime = DateTime.UtcNow,
                 FirstTimeUser = true,
                 SecurityAnswers = securityQuestionAccountListObj
             };
-            accountRepository.Insert(account);
+            _accountRepository.Insert(account);
 
             return Ok();
         }
@@ -105,68 +117,11 @@ namespace ECS.WebAPI.Controllers
         [EnableCors(origins: "http://localhost:8080", headers: "*", methods: "GET")]
         public IHttpActionResult GetSecurityQuestions()
         {
+            List<SecurityQuestion> allQuestions;
+            allQuestions = _securityQuestionRepository.GetAllQuestions();
 
-            // Insert the model into Ok() for it to return in the body as a JSON.
-
-            // TEST ????
-            List<SecurityQuestion> test = new List<SecurityQuestion> {
-                new SecurityQuestion{SecurityQuestionID = 1, SecQuestion = "Who?" },
-                new SecurityQuestion{SecurityQuestionID = 2, SecQuestion = "What?" },
-                new SecurityQuestion{SecurityQuestionID = 3, SecQuestion = "When?" },
-                new SecurityQuestion{SecurityQuestionID = 4, SecQuestion = "Why?" }
-            };
-            
-            return Content(HttpStatusCode.OK, new JavaScriptSerializer().Serialize(test));
+            // TODO: @Trish change to Ok()
+            return Content(HttpStatusCode.OK, new JavaScriptSerializer().Serialize(allQuestions));
         }
     }
 }
-
-// Proccess any other information.
-//if (ModelState.IsValid)
-//{
-//    // Check SSO DB for User.
-//    //PostRegistrationToSSO(userAccount.Username);
-
-//    // If successful, save user to app DB. If not successful, reject registration.
-//    using (ECS.ECSContext.ECSContext context = new ECS.ECSContext.ECSContext())
-//    {
-//        context.Accounts.Add(new Account
-//        {
-//            UserName = userAccount.Username,
-//            Password = HashService.HashPasswordWithSalt(userAccount.Password, HashService.CreateSaltKey()), //ConfirmPassword = userAccount.ConfirmPassword
-//            SecurityAnswers = new List<SecurityQuestionAccount>
-//            {
-//                new SecurityQuestionAccount
-//                {
-//                    Answer = userAccount.SecurityAnswers.ElementAt(0),
-//                    SecurityQuestionID = userAccount.SecurityQuestions.ElementAt(0)
-//                },
-//                new SecurityQuestionAccount
-//                {
-//                    Answer = userAccount.SecurityAnswers.ElementAt(1),
-//                    SecurityQuestion = userAccount.SecurityQuestions.ElementAt(1)
-//                },
-//                new SecurityQuestionAccount
-//                {
-//                    Answer = userAccount.SecurityAnswers.ElementAt(2),
-//                    SecurityQuestion = userAccount.SecurityQuestions.ElementAt(2)
-//                }
-//            }
-//        });
-//        context.Users.Add(new User
-//        {
-//            Email = userAccount.Email,
-//            FirstName = userAccount.FirstName,
-//            LastName = userAccount.LastName,
-//            Address = userAccount.Address
-//        });
-//        context.ZipLocations.Add(new ZipLocation
-//        {
-//            ZipCode = userAccount.ZipCode,
-//            City = userAccount.City,
-//            State = userAccount.State
-//        });
-//    }
-//    context.SaveChanges();
-//    // return RedirectToAction();
-//}
