c2aadb9d6eb68e26c2ae9a9650c01a736fcde286
Hugo Garcia <hugogarcia354@gmail.com>
2018-04-09 14:04:25 -0700

    merge

--- ECSDevServer/ECS.WebAPI/Controllers/v1/HomeController.cs
+++ ECSDevServer/ECS.WebAPI/Controllers/v1/HomeController.cs
@@ -13,7 +13,7 @@ using ECS.WebAPI.Filters.AuthorizationFilters;
 namespace ECS.WebAPI.Controllers.v1
 {
 
-    [RoutePrefix("Home")]
+    [RoutePrefix("v1/Home")]
     // Had to make a custom filter for RequireHttpsAttribute
     [RequireHttps]
     public class HomeController : ApiController
