d7fa8ca3b1b77d1d867d3e8480b69bd62010a989
FurkanArif <you@example.com>
2018-04-15 17:50:41 -0700

    SweepstakeEntry
    Did a GET request for user points
    Still Working On It
    MY CODE IS HEAVILY COMMENTED OUT
    STILL WORKING ON IT

--- ECSDevClient/src/pages/Sweepstake.vue
+++ ECSDevClient/src/pages/Sweepstake.vue
@@ -1,51 +1,100 @@
 <template>
   <div>
+    <!-- GET POINTS FIRST FROM USER ACCOUNT AND SOLVE THE ISSUE OF GETTING SCHOLAR POINTS -->
+    <!-- thinking of changing the cost to the ticket number, which i can use to get the max number of tickets -->
     <h1>Welcome to Sweepstake Entry. Buy A Ticket To Enter Into The Sweepstake.</h1>
     <h1>Time Left</h1>
     <Countdown deadline="April 22, 2018"></Countdown>
     <!-- or
     <Countdown end="August 22, 2022"></Countdown> -->
-    <button v-on:click="ticketBought(totalPoints,ticketPrice),timeStamp(timeDateStamp)">Buy A Ticket</button>
-    <div class="timeStamp">
-    <!-- <h2>{{new Date() | moment("dddd, MMMM Do YYYY , hh:mm:ss a")}}</h2> -->
-    <!-- <button v-on:click="timeStamp(timeDateStamp)">Create Time Stamp in UTC</button> -->
-    </div>
+    <button v-on:click="fetchUserInfo(username,Points)">Display Your Points First</button>
+    <!-- <button v-on:click="fetchUserInfo(username,Points),ticketBought(Points,Cost)">Buy A Ticket</button> -->
+    <button v-on:click="ticketBought(Points,Cost)">Buy A Ticket</button>
   </div>
 </template>
 <script>
 import Vue from 'vue'
 import moment from 'moment'
-// timestamp pattern 2018-03-06 16:46:08.433 use the UTC only
-// sweepstake main in which we have sweepstake id, date open and close, prize, username who wins.
-// now we have a seperate sweepstake entry in which we have a ticket purchase timestamp, the cost, the username of who bought the ticket,
-// sweepstake open date like when a sweepstake ticket can be bought the date open and close. we need an isActive flag too.
 import Countdown from 'vuejs-countdown'
+// import store from '@/store/index'
+import axios from 'axios'
 Vue.use(require('vue-moment'))
 export default {
   name: 'prizes',
   data () {
     return {
-      totalPoints: 10,
-      ticketPrice: 5,
-      timeDateStamp: ' '
+      Points: '', // Points i get from the user account
+      Cost: 0, // it is the Price of ticket from sweepstake admin
+      username: this.$store.getters.getUsername,
+      timeDateStamp: moment().utc('dddd, MMMM Do YYYY , hh:mm:ss').format() // the time date stamp is right in utc format
+      // SweepStakesID: '7',
+      // OpenDateTime: moment().utc('dddd, MMMM Do YYYY , hh:mm:ss').format()
     }
   },
   components: { Countdown },
   methods: {
-    ticketBought: function (totalPoints, ticketPrice) {
-      if (totalPoints >= ticketPrice) {
-        alert('Congragulations! One ticket bought for ' + ticketPrice + ' points!')
-        totalPoints = totalPoints - ticketPrice
-        alert('Points left = ' + totalPoints)
-        return totalPoints
+    fetchUserInfo: function (username, Points) {
+      axios({
+        method: 'GET',
+        url: this.$store.getters.getBaseAppUrl + 'v1/Sweepstake/ScholarPoints/' + username,
+        headers: this.$store.getters.getRequestHeaders
+      })
+        .then(response => {
+          console.log(response.data)
+          this.Points = response.data
+          alert('Points are ' + this.Points)
+          // return Points
+          // JSON.parse(response.data)
+          // this.$data.points = JSON.parse(response.data)
+          // console.log('Points', response.data)
+          // response.data.Points
+          // console.log(response.Points)
+        })
+        .catch(error => {
+          console.log(error.response)
+          this.$data.error = JSON.parse(error.response.data)
+          if (error.response.status === 500) {
+            alert('We apologize.  We are unable to process your request at this time.')
+          }
+        })
+    },
+    ticketBought: function (Points, Cost) {
+      if (Points >= Cost) {
+        alert('Congragulations! One ticket bought for ' + Cost + ' points! Your Points are ' + Points)
+        Points = Points - Cost
+        alert('Points left = ' + Points)
+        // axios({
+        //   // get request for sweepstake id and open date time from the sweepstake admin database table
+        //   method: 'POST',
+        //   // url: this.$store.getters.getBaseAppUrl + 'v1/SweepstakeAdmin/submitSweepstake',
+        //   url: 'https://localhost:44311/v1/Sweepstake/submitSweepstake',
+        //   headers: store.getters.getRequestHeaders,
+        //   data: {
+        //     // 'SweepstakesID': this.$data.SweepStakesID,
+        //     // 'OpenDateTime': this.$data.timeDateStamp, // change it later to open date time
+        //     'PurchaseDateTime': this.$data.timeDateStamp,
+        //     'Cost': this.$data.Cost
+        //     // 'Price': this.$data.Price,
+        //     // 'username': this.$data.username
+        //   }
+        // })
+        //   .then(response => {
+        //     console.log(response)
+        //     this.$router.push({
+        //       name: 'Sweepstake'
+        //       // params: { isSuccess: true }
+        //     })
+        //   })
+        //   .catch(error => {
+        //     console.log(error.response)
+        //     this.$data.error = JSON.parse(error.response.data)
+        //     if (error.response.status === 500) {
+        //       alert('We apologize.  We are unable to process your request at this time.')
+        //     }
+        //   })
       } else {
         alert('Sorry! You have Insufficient Points!')
       }
-    },
-    timeStamp: function (timeDateStamp) {
-      timeDateStamp = moment().utc('dddd, MMMM Do YYYY , hh:mm:ss').format()
-      alert(timeDateStamp)
-      return timeDateStamp
     }
   }
 }
