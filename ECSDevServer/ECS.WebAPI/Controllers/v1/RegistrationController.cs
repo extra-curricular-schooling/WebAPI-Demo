74113151582e756a0b3f253b5467d9cb01dea8bc
Scott Roberts <s.e.roberts0@gmail.com>
2018-04-26 13:49:55 -0700

    Duplicate Constructor Removed

--- ECSDevServer/ECS.WebAPI/Controllers/v1/RegistrationController.cs
+++ ECSDevServer/ECS.WebAPI/Controllers/v1/RegistrationController.cs
@@ -19,15 +19,9 @@ namespace ECS.WebAPI.Controllers.v1
         // TODO: @Scott Try to make the controller logic follow interfaces (like a transformer: fetch and send)
         private readonly RegistrationControllerLogic _controllerLogic;
         
-
         public RegistrationController()
         {
             _controllerLogic = new RegistrationControllerLogic();
-        }
-
-        public RegistrationController(RegistrationControllerLogic controllerLogic)
-        {
-            _controllerLogic = controllerLogic;
         }
 
         public RegistrationController(RegistrationControllerLogic registrationLogic)
